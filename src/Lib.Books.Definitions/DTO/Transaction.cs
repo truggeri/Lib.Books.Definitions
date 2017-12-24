e95ce73f78569a66042fb41a6c9fa0830d6fb65b
Thomas Ruggeri <truggeri@gmail.com>
2017-12-23 22:09:09 -0500

    Creating transaction DTO.

---
+++ src/Lib.Books.Definitions/DTO/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NodaMoney;
+
+namespace Lib.Books.Definitions
+{
+    public class Transaction
+    {
+        public Guid id { get; }
+        public Money Amount { get; set; }
+        public string Description { get; set; }
+        public string FromAccount { get; set; }
+        public string ToAccount { get; set; }
+        public DateTime RecordedAt { get; set; }
+        public string Idendifier { get; set; }
+    }
+}
